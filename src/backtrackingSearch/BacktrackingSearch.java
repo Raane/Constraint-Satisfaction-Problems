0c9bf5ef4e3ade85b5a89a233ffb789f6baf13d1
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-18 02:51:58 +0200

    Making a test for the state.

--- src/backtrackingSearch/BacktrackingSearch.java
+++ src/backtrackingSearch/BacktrackingSearch.java
@@ -1,5 +1,17 @@
 package backtrackingSearch;
 
 public class BacktrackingSearch {
-	
+	public static void main(String[] args) {
+		State state = new State(8);
+		System.out.println(state);
+		System.out.println("");
+		state.addQueen();
+		System.out.println(state);
+		System.out.println("");
+		state.addQueen();
+		System.out.println(state);
+		System.out.println("");
+		state.removeQueen();
+		System.out.println(state);
+	}
 }
