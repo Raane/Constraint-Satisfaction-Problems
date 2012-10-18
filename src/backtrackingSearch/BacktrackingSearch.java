706f313d62fbc3b7e930285384e680c1821a7b54
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-18 03:43:08 +0200

    Making the shell for the backtrackingSearchAlgorithm.

--- src/backtrackingSearch/BacktrackingSearch.java
+++ src/backtrackingSearch/BacktrackingSearch.java
@@ -1,8 +1,17 @@
 package backtrackingSearch;
 
 public class BacktrackingSearch {
+	
+	public BacktrackingSearch() {
+	}
+	
+	public State solve(int boardSize) {
+		return null;
+	}
+	
 	public static void main(String[] args) {
-
+		BacktrackingSearch search = new BacktrackingSearch();
+		search.solve(8);
 	}
 }
 	
