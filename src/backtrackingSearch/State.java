6b22efb053e7aa05a420dc5c1b13b43b9f64cbf8
Rune Holmgren <Raane.Holm@gmail.com>
2012-10-18 02:32:16 +0200

    Finished the addQueen(), but no testing is done yet.

--- src/backtrackingSearch/State.java
+++ src/backtrackingSearch/State.java
@@ -12,8 +12,8 @@ import java.util.ArrayList;
 public class State {
 
 	private int boardSize;	// Size of the board
-	private ArrayList<ArrayList<Integer>> D;		// Queens
-	private ArrayList<ArrayList<ArrayList<Integer>>> C;		// Collisions
+	private ArrayList<ArrayList<Integer>> D;							// Queens
+	private ArrayList<ArrayList<ArrayList<Integer>>> C;					// Collisions
 	private ArrayList<Integer> queensPlaced = new ArrayList<Integer>();
 	
 	public State(int boardSize) {	// Create an empty boardstate
@@ -41,24 +41,39 @@ public class State {
 		
 		int queen = getQueenToAdd();
 		int field = getFieldForQueenPlacement(queen);
-		
+		                   
 		for(int i=0;i<boardSize;i++) {			
 			C.get(i).add(new ArrayList<Integer>());
 		}
 		
+		//First we add all collitions
+		
 		// Doing the operations on the row
-		for(int collition:D.get(queen)) {
-			if(collition!=queen) C.get(queen).get(C.get(queen).size()-1).add(collition);
-			D.get(queen)
-		}
+		for(int collition:D.get(queen)) if(collition!=field) C.get(queen).get(C.get(queen).size()-1).add(collition);	// Add new collitions in an (at first) empty arraylist
+		//D.get(queen).clear();									// Clear the row in the queen table
+		//D.get(queen).add(field);								// Add the queen to the now empty list of possible queen locations in the row
+		
 		// Doing the operations on the column
-		for(int i=0;i<boardSize;i++){
-			if(i!=queen && D.get(i).contains(queen)) C.get(i).get(C.get(i).size()-1).add(queen);
+		for(int i=0;i<boardSize;i++) if(i!=queen && D.get(i).contains(field)) C.get(i).get(C.get(i).size()-1).add(field);
+		
+		// Doing operations on the diagonals
+		for(int i=0;i<boardSize;i++) {
+			for(int j=-1;j<1;j+=2) {
+				//int x = (i-queen)*j;
+				if((i-queen)*j>=0 && (i-queen)*j < boardSize && i!=queen && D.get(i).contains((i-queen)*j));
+			}
 		}
+		
+		// Fjerner colisjonene fra D
+		for(int i=0;i<boardSize;i++) {
+			for(int collition:C.get(i).get(C.get(i).size()-1)) {
+				D.get(i).remove((Integer)collition);				//TODO Virker dette egentlig med Integer?
+			}
+		}
+		
 		return true; // hey! It worked!
 	}
 
-	
 
 	/*
 	 * Using methods for these two tasks are overkill, but I will be able to change them to something better later on without much extra work.
